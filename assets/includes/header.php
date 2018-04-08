67e2f17d5a96612c922cea56d3ad629db0f8270e
abolzz <abolzzy@gmail.com>
2018-04-08 22:14:14 +0200

    Statistics & back button
    -improved the graph;
    -added the sum;
    
    -back button functionality

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -1,9 +1,7 @@
 <header class="header">
-	<div class="button--back">
-		<a href="index.php">
+	<button id="backButton" class="button--back" index="4" data-value="Home" source="img/home-icon.png">
 			<img src="img/back-icon.png" alt="Back Icon">
-		</a>
-	</div>
+	</button>
 	<div class="header--heading">
 		<h1 id="header--heading-h1">Home</h1>
 		<img id="header--heading-icon" src="img/home-icon.png" alt="Home Icon">
--- js/navigation.js
+++ js/navigation.js
@@ -4,32 +4,47 @@ var navItems = document.getElementsByClassName('nav--ul-li'),
 	navItemsMonth = document.getElementsByClassName('nav--ul-li-months'),
 	heading = document.getElementById('header--heading-h1'),
 	headingIcon = document.getElementById('header--heading-icon'),
-	sections = document.getElementsByTagName('section');
+	sections = document.getElementsByTagName('section'),
+	backButton = document.getElementById('backButton');
 
 function navigate (array) {
-	for (var i = 0; i < array.length; i++) {
-	    array[i].onclick = function(event) {
+		for (var i = 0; i < array.length; i++) {
+		    array[i].onclick = function(event) {
 
-	    	// Highlights the active nav icon
-	    	for (var i = 0; i < array.length; i++) {
-	    		array[i].classList.remove('active');
-	    	}
-	    	this.classList.add('active');
+		    	// Highlights the active nav icon
+		    	for (var i = 0; i < array.length; i++) {
+		    		array[i].classList.remove('active');
+		    	}
+		    	this.classList.add('active');
 
-	    	// If happens on main nav then change the heading as well
-	    	if (array == navItems) {
-		    	heading.innerHTML = this.getAttribute('data-value');
-		    	headingIcon.src = this.getAttribute('source');
-		    	var index = this.getAttribute('index');
-		    	for (var i = 0; i < sections.length; i++) {
-	    			sections[i].classList.add('d-none');
-	    		}
-	    		sections[index].classList.remove('d-none');
-	    		sections[index].classList.add('active');
-	    	}
-	    }
-	}
+		    	// If happens on main nav then change the heading
+		    	if (array == navItems) {
+			    	heading.innerHTML = this.getAttribute('data-value');
+			    	headingIcon.src = this.getAttribute('source');
+			    	var index = this.getAttribute('index');
+			    	for (var i = 0; i < sections.length; i++) {
+		    			sections[i].classList.add('d-none');
+		    		}
+		    		sections[index].classList.remove('d-none');
+		    		sections[index].classList.add('active');
+		    	}
+		    }
+		}
 }
 
 navigate(navItems);
 navigate(navItemsMonth);
+
+backButton.onclick = function(event) {
+	for (var i = 0; i < sections.length; i++) {
+		sections[i].classList.add('d-none');
+	}
+	sections[4].classList.remove('d-none');
+	sections[4].classList.add('active');
+	heading.innerHTML = backButton.getAttribute('data-value');
+	headingIcon.src = backButton.getAttribute('source');
+	for (var i = 0; i < navItems.length; i++) {
+		navItems[i].classList.remove('active');
+	}
+	navItems[4].classList.add('active');
+}
